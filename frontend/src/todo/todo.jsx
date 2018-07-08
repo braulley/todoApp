dd98519d94d106bce37bd035a85d525e31500209
braulley <juniorbraulley@gmail.com>
2018-07-08 18:23:27 -0300

    fix template

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageheader'
+import TofoForm from '../todo/todoForm'
+import TodoList from '../todo/todoList'
 
 export default class Todo extends Component {
+
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd(){
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' email='Cadastro'> </PageHeader>
+                <TofoForm handleAdd={this.handleAdd} />
+                <TodoList />
             </div>
         )
     }
